56eebd1fa3d0a3518742f496a2baed19be9ec1db
Sharon Byegon <byegon.sharon@gmail.com>
2024-03-28 17:19:37 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+// grabbing the dodger
+const dodger = document.getElementById('dodger');
+
+// setting the initial position of the dodger
+dodger.style.left = '0px';
+
+// function to move the dodger left
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+// function to move the dodger right
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 340) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+//event listener
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+})
